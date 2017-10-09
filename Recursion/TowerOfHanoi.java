b2d4ede13dde0e54dd23e92a7bb673d0e941bf46
Kumud Bhat <kumudganeshbhat@gmail.com>
2017-10-09 14:28:45 -0400

    Update TowerOfHanoi.java

--- Recursion/TowerOfHanoi.java
+++ Recursion/TowerOfHanoi.java
@@ -9,12 +9,18 @@ import java.util.Scanner;
  * 2. Move n disks from tower X to tower Y using tower Z
  * 3. Only one disk can be moved at a time from one tower to other tower and
  * 4. Heavier disk should not be placed on lighter disk
+ *
+ * No of recursive calls = O(2^n+1 -1)
+ * No of movements = O(2^n -1)
+ * Space complexity = O(n)
+ * Time complexity = O(2^n) - NP Completeness/Hard category
  * 
  * @author Kumud Bhat
  * @version 1.0
  */
 
 public class TowerOfHanoi{
+    
     //instance variables
     int n; //number of disks
     static String source = "X"; //tower X
@@ -22,7 +28,7 @@ public class TowerOfHanoi{
     static String intermediate = "Z"; //tower Z
     
     public void moveDisks(int n, String source, String destination, String intermediate){
-        if(n>1){
+        if(n>1){ //Reducing number of invocations by changing condition
             moveDisks(n-1,source,intermediate,destination);
             System.out.println("Move disk" + " " + n + "from " + source + "to " + destination );
             moveDisks(n-1,intermediate,destination,source);
@@ -31,9 +37,8 @@ public class TowerOfHanoi{
             System.out.println("Move disk" + " " + n + "from " + source + "to " + destination );
         else
             return;
-        
-        
     }
+    
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         TowerOfHanoi toh = new TowerOfHanoi();
@@ -43,7 +48,5 @@ public class TowerOfHanoi{
         
         toh.n = sc.nextInt();
         toh.moveDisks(toh.n,source,destination,intermediate);
-        
-        
-    }
+  }
 }
